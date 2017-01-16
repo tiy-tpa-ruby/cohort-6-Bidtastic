f0112d0734fc5a6cf6dc24bc183b3ef447ac8c09
Kenia Valladarez <keniavalladarez@gmail.com>
2017-01-16 11:28:36 -0500

    Congirues the methods and routes for the session controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get  '/auth/:provider'          => 'omniauth#auth', as: :auth
+  get  '/auth/:provider/callback' => 'session#create'
+  get  '/auth/failure'            => 'session#failure'
+
+  get  '/login'                   => 'session#new'
+  post '/login'                   => 'session#create'
+  get  '/logout'                  => 'session#destroy'
 end
