9b1cb7361b8d973f45152bb8d8913c501492f9b3
Jonathan Hutson <jhut110766@gmail.com>
2017-01-18 16:54:41 -0500

    filing logic

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,9 @@ Rails.application.routes.draw do
   end
   resources :events
   resources :items
-  root "items#index"
+
+  root "events#index"
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get  '/auth/:provider'          => 'omniauth#auth', as: :auth
   get  '/auth/:provider/callback' => 'session#create'
