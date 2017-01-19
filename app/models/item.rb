c5bed90c0b0db0a0b94f4f59814ecb1c555c1564
Jonathan Hutson <jhut110766@gmail.com>
2017-01-19 09:24:43 -0500

    drop down filing

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,12 @@
 class Item < ApplicationRecord
+  PRICES = [
+    ["", 0],
+    ["$", 1],
+    ["$$", 2],
+    ["$$$", 3],
+    ["$$$$", 4],
+    ["$$$$$", 5]
+  ]
   belongs_to :user
   belongs_to :event
   has_many :pictures
